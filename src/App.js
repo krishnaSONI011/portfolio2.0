180ec13fe91df4f490e7f3a955a9aee3e72b3200
krishna soni <soni01krishna@gmail.com>
2023-09-27 00:22:13 +0530

    navbar component and dark mode done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import {DarkmodeProvider} from './Components/context/Darkmode.js'
 function App() {
   return (
    <>
+   <DarkmodeProvider>
+
    <Navbar/>
+   </DarkmodeProvider>
    </>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from "react";
-
+import React,{useEffect} from "react";
+import { useDarkmode } from "./context/Darkmode";
 export default function Navbar() {
+    let {darkmode,toggleDarkMode} = useDarkmode();
+    
+    useEffect(()=>{
+        document.body.style.backgroundColor = darkmode ? '#191919' : 'white'
+    },[darkmode])
+
   return (
-    <div className="w-100 sticky py-3 shadow-md">
-      <div className="flex justify-between text-lg px-7">
+    <div className={`w-100 sticky py-3 shadow-md ${darkmode ? 'bg-[#232323]' : 'bg-white'} ${darkmode ? 'text-white' : 'text-black'}`} >
+      <div className="flex justify-between text-lg px-12">
         {/* Name logo */}
         <div className="logo font-1 font-semibold text-2xl">
           <span className="text-[#6D83F2] ">&lt;</span>Krishna
@@ -13,20 +19,20 @@ export default function Navbar() {
         {/* nav links */}
 
         <div className="flex">
-          <ul className="flex text-[#6A98F0] font-1">
-            <li className="hover:text-[#383838] cursor-pointer px-3">Home</li>
-            <li className="hover:text-[#383838] cursor-pointer px-3">Skill</li>
-            <li className="hover:text-[#383838] cursor-pointer px-3">
+          <ul className="flex text-[#6A98F0] font-1 mr-10">
+            <li className={` ${darkmode ? 'hover:text-white' : 'hover:text-[#383838]'} cursor-pointer px-3`}>Home</li>
+            <li className={`${darkmode ? 'hover:text-white' : 'hover:text-[#383838]'} cursor-pointer px-3`}>Skill</li>
+            <li className={` ${darkmode ? 'hover:text-white' : 'hover:text-[#383838]'} cursor-pointer px-3`}>
               Project
             </li>
-            <li className="hover:text-[#383838] cursor-pointer px-3 ">
+            <li className={` ${darkmode ? 'hover:text-white' : 'hover:text-[#383838]'} cursor-pointer px-3`}>
               Contact us
             </li>
           </ul>
-          <div className="bg-[#C7D0FF] relative rounded-2xl px-10">
+          <div className={` ${darkmode ? "bg-[#383838]" : "bg-[#C7D0FF]"} relative rounded-2xl px-10`} >
              
-            <div className=" flex items-center justify-center absolute left-1 bg-[#6D83F2] rounded-3xl w-10 text-xs top-[3px] bottom-0 py-1 text-white font-semibold cursor-pointer h-[27px]">
-                   DARK
+            <div className={`flex items-center justify-center absolute ${darkmode ? 'left-9' :'left-1'} bg-[#6D83F2] rounded-3xl w-10 text-xs top-[3px] bottom-0 py-1 text-white font-semibold cursor-pointer h-[27px] transition-all`} onClick={toggleDarkMode}  >
+                   {darkmode ?"LIGHT" :'DARK'}
             </div>
           </div>
         </div>
