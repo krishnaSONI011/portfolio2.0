36c34c6dba4cc9cc250d1a8ef0f103f04e931fde
krishna soni <soni01krishna@gmail.com>
2024-01-18 12:04:50 +0530

    change in get in touch

--- src/Components/GetinTouch.jsx
+++ src/Components/GetinTouch.jsx
@@ -3,15 +3,15 @@ import { MdHandshake } from "react-icons/md";
 import Form from './Form';
 export default function GetinTouch() {
   return (
-    <div className='mt-10 px-40'>
+    <div className='mt-10 md:px-40'>
       <div className='text-center text-3xl font-semibold text-[#6A98F0] font-1'>
         Get In Touch
       </div>
 
-     <div className='flex '>
+     <div className='md:flex '>
         <div className='py-10'>
 
-        <div className='w-[400px] h-[400px] bg-[#6A98F0] flex justify-center items-center flex-col rounded-l-xl'>
+        <div className='w-full md:w-[400px] h-[400px] bg-[#6A98F0] flex justify-center items-center flex-col rounded-l-xl'>
                 <div>
                     <MdHandshake className='text-9xl text-white'/>
                 </div>
